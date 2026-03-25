12cf8ded0709421eec0b4a05ffb6c10ee5e011c7
Guoxiong Li <lgxbslgx@gmail.com>
2026-03-25 11:10:38 +0800

    Fix bug of CallSiteInfo
    Signed-off-by: Guoxiong Li <lgxbslgx@gmail.com>

--- src/hotspot/cpu/riscv/jeandleCompiledCode_riscv.cpp
+++ src/hotspot/cpu/riscv/jeandleCompiledCode_riscv.cpp
@@ -122,10 +122,7 @@ bool JeandleCompiledCode::pd_resolve_reloc(JeandleAssembler& assembler,
         int inst_end_offset = JeandleAssembler::fixup_call_inst_offset(static_cast<int>(block->getAddress().getValue() + edge.getOffset()));
 
         // TODO: Set the right bci.
-        CallSiteInfo* call_info = new CallSiteInfo(JeandleCompiledCall::ROUTINE_CALL,
-                                                   target_addr,
-                                                   -1/* bci */,
-                                                   target_addr == JeandleRuntimeRoutine::get_routine_entry("uncommon_trap")/* has_deopt_operands */);
+        CallSiteInfo* call_info = new CallSiteInfo(JeandleCompiledCall::ROUTINE_CALL, target_addr, -1/* bci */);
         if (JeandleRuntimeRoutine::is_gc_leaf(target_addr)) {
           relocs.push_back(new JeandleCallReloc(inst_end_offset, _env, _method, nullptr /* no oopmap */, call_info));
         } else {
@@ -140,9 +137,7 @@ bool JeandleCompiledCode::pd_resolve_reloc(JeandleAssembler& assembler,
         int inst_end_offset = JeandleAssembler::fixup_call_inst_offset(static_cast<int>(block->getAddress().getValue() + edge.getOffset()));
 
         // TODO: Set the right bci.
-        CallSiteInfo* call_info = new CallSiteInfo(JeandleCompiledCall::EXTERNAL_CALL,
-                                                   target_addr,
-                                                   -1/* bci */);
+        CallSiteInfo* call_info = new CallSiteInfo(JeandleCompiledCall::EXTERNAL_CALL, target_addr, -1/* bci */);
         // LLVM doesn't rewrite intrinsic calls to statepoints, so we don't need oopmaps for external calls.
         relocs.push_back(new JeandleCallReloc(inst_end_offset, _env, _method, nullptr /* no oopmap */, call_info));
       } else if (JeandleAssembler::is_section_word_reloc(edge, rel_high_edges)) {
